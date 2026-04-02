21c8088eda90dcc0d75a6b3d9a0888215be5ad53
Benjamin Inglis <bjammin134@gmail.com>
2026-04-02 13:16:59 -0700

    Implement Postgres get_latest method

--- src/app/database/db_strategy.py
+++ src/app/database/db_strategy.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from abc import ABC, abstractmethod
 import pymongo
+from sqlalchemy import select, desc
 
 from src.app.database.postgres_db import SessionLocal
 from src.app.models.crypto_price import CryptoPrice
@@ -35,7 +36,7 @@ class MongoDBStorage(StorageStrategy):
     def get_latest(self, symbol: str) -> dict | None:
         db = MongoDBConnection().get_connection()["crypto_db"]
         res = db["crypto_prices"].find_one(
-            {"symbol": symbol}, sort={"timestamp": pymongo.ASCENDING}
+            {"symbol": symbol}, sort={"timestamp": pymongo.DESCENDING}
         )
         return res
 
@@ -48,13 +49,14 @@ class PostgresStorage(StorageStrategy):
             session.commit()
 
     def get_latest(self, symbol: str) -> dict | None:
-        try:
-            query = f"""SELECT * FROM crypto_prices WHERE symbol = '{symbol}' ORDER BY timestamp ASC """
-            with self._conn.cursor as cursor:
-                row = cursor.execute(query).fetchone()
-                return row
-        finally:
-            self._postgres._pool.putconn(self._conn)
+        with SessionLocal() as session:
+            stmt = (
+                select(CryptoPrice)
+                .where(CryptoPrice.symbol == symbol)
+                .order_by(desc(CryptoPrice.timestamp))
+                .limit(1)
+            )
+            return session.execute(stmt).scalars().first()
 
 
 # --- Context ---
