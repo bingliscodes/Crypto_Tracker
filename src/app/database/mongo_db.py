5908307cac71c278b5973a44047f2b648f71c429
Benjamin Inglis <bjammin134@gmail.com>
2026-04-02 09:23:21 -0700

    Create MongoDB Singleton instance

--- src/app/database/mongo_db.py
+++ src/app/database/mongo_db.py
@@ -1,12 +1,26 @@
 from pymongo import MongoClient
+from src.app.utils.singleton import SingletonMeta
+
+
+class MongoDBConnection(metaclass=SingletonMeta):
+    def __init__(self, uri: str):
+        self._connection = MongoClient(uri)
+
+    def get_connection(self):
+        return self._connection
+
+    def close_connection(self):
+        self._connection.close()
+        self._connection = None
+
 
 uri = "mongodb://localhost:27017"
-client = MongoClient(uri)
+client = MongoDBConnection(uri)
 
 try:
-    client.admin.command("ping")
+    client._connection.admin.command("ping")
     print("Connected successfully")
 except Exception as e:
     print("Connection failed:", e)
 finally:
-    client.close()
+    client._connection.close()
